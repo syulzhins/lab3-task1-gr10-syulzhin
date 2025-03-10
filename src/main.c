f8814b21a6f520a99ee264cf474a667907f6352d
Savely Syulzhin <154425593+syulzhins@users.noreply.github.com>
2025-03-10 07:16:20 +0000

    Реализована основная функция и ввод данных о студентах

--- src/main.c
+++ src/main.c
@@ -82,3 +82,57 @@ void print_young_students(GROUP students[], int n) {
         printf("Студентов в возрасте до 20 лет нет.\n");
     }
 }
+int main() {
+    // Часть 1: Работа со строкой
+    char sentence[100];
+    printf("Введите предложение: ");
+    fgets(sentence, sizeof(sentence), stdin);
+    sentence[strcspn(sentence, "\n")] = 0; // Удаляем символ новой строки
+
+    int removed_count = remove_even_positions(sentence);
+    printf("Измененная строка: %s\n", sentence);
+    printf("Количество удаленных букв: %d\n", removed_count);
+
+    // Часть 2: Работа со структурой GROUP
+    GROUP students[MAX_STUDENTS];
+
+    // Ввод данных о студентах
+    for (int i = 0; i < MAX_STUDENTS; i++) {
+        printf("Введите данные для студента %d:\n", i + 1);
+        printf("Фамилия и инициалы: ");
+        fgets(students[i].Name, sizeof(students[i].Name), stdin);
+        students[i].Name[strcspn(students[i].Name, "\n")] = 0; // Удаляем символ новой строки
+
+        // Ввод даты с проверкой
+        while (1) {
+            printf("Дата поступления (гггг мм дд): ");
+            scanf("%d %d %d", &students[i].DAT.year, &students[i].DAT.month, &students[i].DAT.day);
+            if (is_valid_date(students[i].DAT.year, students[i].DAT.month, students[i].DAT.day)) {
+                break; // Если дата корректная, выходим из цикла
+            } else {
+                printf("Некорректная дата. Пожалуйста, введите снова.\n");
+            }
+        }
+
+        printf("Успеваемость (3 оценки): ");
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &students[i].SES[j]);
+        }
+        getchar(); // Удаляем символ новой строки после ввода
+    }
+
+    // Сортировка студентов по успеваемости
+    sort_students(students, MAX_STUDENTS);
+
+    // Вывод отсортированных данных
+    printf("\nСтуденты, отсортированные по успеваемости:\n");
+    for (int i = 0; i < MAX_STUDENTS; i++) {
+        printf("Студент: %s, Успеваемость: %d %d %d\n", students[i].Name, students[i].SES[0], students[i].SES[1], students[i].SES[2]);
+    }
+
+    // Вывод студентов до 20 лет
+    printf("\nСтуденты, возраст которых не превышает 20 лет:\n");
+    print_young_students(students, MAX_STUDENTS);
+
+    return 0;
+}
